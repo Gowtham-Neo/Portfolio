050fb4ceb657db886719d9600dd2c6c020527b96
Gowtham-Neo <s.gowtham.engr@gmail.com>
2024-07-30 15:03:33 +0530

    new Project added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,7 @@ import FoodApp from "../assets/FoodApp.png";
 import TodoApp from "../assets/Todo-app.png";
 import CGPA from "../assets/CGPA.png";
 import SmartTasker from "../assets/SmartTasker.png";
-
+import Sports from "../assets/Sports.png"
 export default function Projects() {
   return (
     <div
@@ -18,7 +18,38 @@ export default function Projects() {
           Projects I have Done
         </h2>
       </div>
-      <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
+      <div className="grid grid-cols-1 gap-1 mx-12 md:grid-cols-3">
+        <div className="p-4 m-5 text-black border shadow-xl md:m-10 rounded-2xl shadow-slate-900">
+          <a href="https://sport-centre.netlify.app/" target="_blank">
+            <img src={Sports} alt="Sports  Image" className="rounded-2xl" />
+          </a>
+          <div>
+            <h1 className="mt-2 font-serif font-semibold md:text-xl text-amber-700">
+              Sports Center
+            </h1>
+            <p className="mt-3 mb-3 font-sans text-sm md:text-base text-zinc-900">
+              Welcome to our React-based Sports Cente. This
+              dynamic platform is designed to provide users with up-to-date
+              sports news articles and live scores for various sports. Whether
+              you're a sports enthusiast, a casual follower,
+              this dashboard has you covered.
+            </p>
+            <a
+              href="https://github.com/Gowtham-Neo/Sports-Center"
+              target="_blank"
+              className="px-4 py-2 mt-3 text-sm text-black border rounded-lg shadow-sm md:font-medium border-hidden bg-amber-600 hover:bg-amber-400 hover:shadow-inner hover:scale-105"
+            >
+              Code
+            </a>
+            <a
+              href="https://sport-centre.netlify.app/"
+              target="_blank"
+              className="px-4 py-2 mt-3 ml-4 text-sm text-black border rounded-lg shadow-sm md:font-medium border-hidden bg-amber-600 hover:bg-amber-400 hover:shadow-inner hover:scale-105"
+            >
+              Live
+            </a>
+          </div>
+        </div>
         <div className="p-4 m-5 text-black border shadow-xl md:m-10 rounded-2xl shadow-slate-900">
           <a
             href="https://learning-management-system-v39t.onrender.com"
@@ -82,36 +113,7 @@ export default function Projects() {
             </a>
           </div>
         </div>
-        <div className="p-4 m-5 text-black border shadow-xl md:m-10 rounded-2xl shadow-slate-900">
-          <a href="https://gowtham-sj8y.onrender.com/" target="_blank">
-            <img src={TodoApp} alt="LMS Image" className="rounded-2xl" />
-          </a>
-          <div>
-            <h1 className="mt-2 font-serif font-semibold md:text-xl text-amber-600">
-              Todo Application{" "}
-            </h1>
-            <p className="mt-3 mb-3 font-sans text-sm md:text-base text-zinc-900">
-              Experience seamless task management with our Node.js-powered ToDo
-              app, backed by a PostgreSQL database. Effortlessly organize and
-              maintain your daily routine tasks for enhanced productivity and
-              efficiency.
-            </p>
-            <a
-              href="https://github.com/Gowtham-Neo/wd201/tree/main/todo-app"
-              target="_blank"
-              className="px-4 py-2 mt-3 text-sm text-black border rounded-lg shadow-sm md:font-medium border-hidden bg-amber-500 hover:bg-amber-400 hover:shadow-inner hover:scale-105"
-            >
-              Code
-            </a>
-            <a
-              href="https://gowtham-sj8y.onrender.com/"
-              target="_blank"
-              className="px-4 py-2 mt-3 ml-4 text-sm text-black border rounded-lg shadow-sm md:font-medium border-hidden bg-amber-500 hover:bg-amber-400 hover:shadow-inner hover:scale-105"
-            >
-              Live
-            </a>
-          </div>
-        </div>
+        
         <div className="p-4 m-5 text-black border shadow-xl md:m-10 rounded-2xl shadow-slate-900">
           <a
             href="https://gowtham-neo.github.io/CGPA-Calculator/"
